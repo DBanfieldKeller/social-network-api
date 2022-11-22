b3757860bc79c2271bbfb6c2def3204b6c63e155
Goat <dpajbk@gmail.com>
2022-11-22 16:30:24 -0500

    added moment js and updated thought model to include date and reaction

--- models/api/Thought.js
+++ models/api/Thought.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose');
+// require moment for date formatting
+const moment = require('moment');
 
 const thoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
         required: true,
+        minLength: 1,
         maxLength: 280
     },
     createdAt: { 
         type: Date,
-        default: Date.now
-    }
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
 
 
 });
