a08333247afbf3e37077a25fd9093cb69a7ef562
Goat <dpajbk@gmail.com>
2022-11-22 16:36:11 -0500

    deleted separate reaction model file, moved schema to thought model file

--- models/api/Thought.js
+++ models/api/Thought.js
@@ -2,6 +2,37 @@ const mongoose = require('mongoose');
 // require moment for date formatting
 const moment = require('moment');
 
+// create reaction schema
+const reactionSchema = new Schema(
+    {
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: ()=> new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+    },
+    {
+    toJSON: {
+        getters: true
+    } 
+    }
+);
+
+// create thought schema
 const thoughtSchema = new mongoose.Schema({
     thoughtText: {
         type: String,
