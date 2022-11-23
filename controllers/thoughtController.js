033c1db199a300d438d90e1bd922a5e9724a0834
Goat <dpajbk@gmail.com>
2022-11-22 19:57:57 -0500

    get thought by id route added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,14 +4,44 @@ const thoughtController = {
     // Create new thought
     createThought(req, res) {
         Thought.create(req.body)
-        .then(({_id}) => {
-            return User.findOneAndUpdate({ _id: req.params.id}, {$push: {thoughts: _id}}, {new: true});
-        })
-        .then(dbThoughtsData => {
-            res.json(dbThoughtsData)
-        })
-        .catch((err) => res.json(err)); 
+            .then(({ _id }) => {
+                return User.findOneAndUpdate({ _id: req.params.id }, { $push: { thoughts: _id } }, { new: true });
+            })
+            .then(() => {
+                res.json({message: 'New thought added'})
+            })
+            .catch((err) => res.json(err));
     },
+
+    // Get all thoughts and reactions
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .populate({ path: 'reactions', select: '-__v' })
+            .select('-__v')
+            .then(dbThoughtsData => res.json(dbThoughtsData))
+            .catch((err) => {
+                console.log(err);
+                res.status(500).json(err);
+            });
+    },
+    // Get thought by ID
+    getThoughtById(req, res) {
+        Thought.findOne({ _id: req.params.id })
+        .populate({path: 'reactions',select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtsData => {
+            if(!dbThoughtsData) {
+            res.status(404).json({message: 'No thoughts with this ID!'});
+            return;
+        }
+        res.json(dbThoughtsData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+
 };
 
 module.exports = thoughtController;
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
 
 const {
-    createThought
+    createThought,
+    getAllThoughts,
+    getThoughtById
 } = require('../../controllers/thoughtController');
 
-// api/thoughts/:userId/
-router.route('/:id').post(createThought)
+// api/thoughts
+router.route('/').get(getAllThoughts);
+
+// api/thoughts/:userId
+router.route('/:id').post(createThought);
+
+// api/thoughts/:thoughtId
+router.route('/:id').get(getThoughtById)
 
 module.exports = router
